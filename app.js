1e2bd9e30cc72b02dc10014f36134c662abef936
Hayk Saakian <fakest.id.ever@gmail.com>
2015-05-27 13:59:07 -0700

    log socket stuff

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ if(process.env.NODE_ENV !== "production"){
 var fs = require('fs')
 var app = require('express')();
 var server = require('http').Server(app);
+console.log('GOOD SOCKET.IO IMPORT')
 var io = require('socket.io')(server);
 var make_redis_client = require('./make_redis_client')
 var session_client = make_redis_client(7)
 var redis_client = make_redis_client(8)
 var cookieParser = require('cookie-parser');
+console.log('GOOD HANDSHAKE SOCKET.IO IMPORT')
 var socketHandshake = require('socket.io-handshake');
 
 var session = require('express-session');
